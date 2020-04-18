7dd34896ecf94268924b3ed852dcbc7afb55faaf
Rafal Mossakowski <rmossakowski@gmail.com>
2020-04-18 17:55:34 +0200

    Add pagination to listings view and template
    Also: add new listings order and filter.
    Allow page to have size of 3 records.

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,14 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 
 from .models import Listing
 
 def index(request):
-  listings = Listing.objects.all()
-  context = {'listings': listings}
+  listings = Listing.objects.order_by('-list_date').filter(is_published = True)
+  paginator = Paginator(listings, 3)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+  context = {'listings': paged_listings}
   return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
