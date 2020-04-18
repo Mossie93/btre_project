244f6172948051311272c3fd6212f6bf996481f7
Rafal Mossakowski <rmossakowski@gmail.com>
2020-04-18 02:07:46 +0200

    Change photos path so it includes model id
    Add not about bug that needs to be fixed

--- realtors/models.py
+++ realtors/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from datetime import datetime
 
+# BUG https://docs.djangoproject.com/en/3.0/ref/models/fields/#django.db.models.FileField.upload_to
+# BUG The instance.id is equal to None when creating new object.
+def realtor_photo_path(instance, filename):
+  return f"realtors/{instance.id}/{filename}"
+
 class Realtor(models.Model):
   name = models.CharField(max_length=200)
-  photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+  photo = models.ImageField(upload_to=realtor_photo_path)
   description= models.TextField(blank=True)
   phone = models.CharField(max_length=20)
   email = models.CharField(max_length=50)
